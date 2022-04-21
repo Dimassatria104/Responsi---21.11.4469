97eaf96033a4dd56d24e835629dd8cbbabd0119e
Dimassatria104 <dimassatria5603@gmail.com>
2022-04-21 10:17:49 +0700

    responsi

---
+++ Karyawan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4469
+{
+    public class Karyawan
+    {
+        public int Nik(int nik)
+        {
+        return nik;
+        }
+
+        public string Nama(string nama)
+        {
+            return nama;
+        }
+
+        public int GajiBulanan(double gaji)
+        {
+            return (int)gaji;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4469
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan();
+          
+
+
+            Console.WriteLine("No Nik/Nama                      Gaji Bulanan");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("1. {0}", karyawan.Nik(190302123), ". {1}", karyawan.Nama("Paijo"), ".                      {2}", karyawan.GajiBulanan(3000000));
+            Console.WriteLine("2. {0}", karyawan.Nik(190302124), ". {1}", karyawan.Nama("Jono"), ".                       {2}", karyawan.GajiBulanan(2000000));
+            Console.WriteLine("Asyiiiiiik  kenaikan gaji 10% !!");
+
+            Console.WriteLine("No Nik/Nama                      Gaji Bulanan");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("1. {0}", karyawan.Nik(190302123), ".  \t{1}", karyawan.Nama("Paijo"), ".                       {2}", karyawan.GajiBulanan(3000000) + karyawan.GajiBulanan(3000000 * 0.1));
+            Console.WriteLine("2. {0}", karyawan.Nik(190302124), ".  \t{1}", karyawan.Nama("Jono"), ".                        {2}", karyawan.GajiBulanan(2000000) + karyawan.GajiBulanan(2000000 * 0.1));
+
+            Console.ReadKey();
+        }
+    }
+    
+}
